b237ada7bdf429e1ca57b95e5702e38f2a18975d
Saurabh <sbj.amb@gmail.com>
2022-07-29 17:43:43 +0530

    Added login form and validation

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from app.forms import LoginForm
 
 
 @app.route("/")
@@ -17,3 +18,12 @@ def index():
         }
     ]
     return render_template("index.html", title="Home", user=user, posts=posts)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f"Login requested for user {form.username.data}, remember_me = {form.remember_me.data}")
+        return redirect(url_for("index"))
+    return render_template("login.html", title="Sign In", form=form)
