4f98137b1d669350acfc0e33232a294adf8b3ae7
Saurabh <sbj.amb@gmail.com>
2022-07-31 11:37:12 +0530

    Added posts table to database

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    posts = db.relationship('Post', backref='Author', lazy='dynamic')
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return f"<User {self.username}>"
