e2ca0d0118c0d1ee177520b2d8c3bffeb0b8aaa5
Saurabh <sbj.amb@gmail.com>
2022-08-02 15:14:21 +0530

    added login manager

--- app/models.py
+++ app/models.py
@@ -1,9 +1,10 @@
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
-from app import db
+from app import db, login
+from flask_login import UserMixin
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -28,3 +29,8 @@ class Post(db.Model):
 
     def __repr__(self):
         return f"<Post {self.body}>"
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
