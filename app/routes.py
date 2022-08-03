1bfc7f93b4ddf35e91a87e48bbe2d61d64161d9c
Saurabh <sbj.amb@gmail.com>
2022-08-03 16:28:29 +0530

    added login manager

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,13 @@
 from app import app
 from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
+from flask_login import current_user, login_user
+from app.models import User
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    user = {"username": "Robin"}
     posts = [
         {
             "author": {"username": "John"},
@@ -17,13 +18,23 @@ def index():
             "post": "Wakanda forever"
         }
     ]
+    if current_user.is_anonymous:
+        return redirect(url_for('login'))
+    else:
+        user = current_user
     return render_template("index.html", title="Home", user=user, posts=posts)
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f"Login requested for user {form.username.data}, remember_me = {form.remember_me.data}")
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for("index"))
     return render_template("login.html", title="Sign In", form=form)
