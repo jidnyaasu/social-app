f915b9653b3a205a8ae8caac8bf8770b2167c606
Saurabh <sbj.amb@gmail.com>
2022-08-06 23:52:58 +0530

    Added user profile page route

--- app/routes.py
+++ app/routes.py
@@ -60,3 +60,14 @@ def register():
         flash("Congratulations! You are now a registered user!")
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/user/<username>")
+@login_required
+def user(username):
+    user = User.query.filter_by(username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
