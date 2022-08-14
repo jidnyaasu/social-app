87a7de8f7cebb00ca1ecf525900ba9dfac33935f
Saurabh <sbj.amb@gmail.com>
2022-08-14 14:35:55 +0530

    Added error handlers and error html pages

--- config.py
+++ config.py
@@ -8,3 +8,9 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = os.getenv("MAIL_SERVER")
+    MAIL_PORT = os.getenv("MAIL_PORT")
+    MAIL_USE_TLS = os.getenv("MAIL_USE_TLS")
+    MAIL_USERNAME = os.getenv("MAIL_USERNAME")
+    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+    ADMINS = []
