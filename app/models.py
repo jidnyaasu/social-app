859b735942206753ec89eb08d67cfcd0254b4c37
Saurabh <sbj.amb@gmail.com>
2022-08-16 23:52:14 +0530

    adding and removing follows

--- app/models.py
+++ app/models.py
@@ -39,6 +39,17 @@ class User(UserMixin, db.Model):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return f"https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}"
 
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(followers.c.followed_id == user.id).count() > 0
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
