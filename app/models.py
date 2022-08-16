158864e761797efb733e9b5072d480d19ff68f1d
Saurabh <sbj.amb@gmail.com>
2022-08-16 23:17:46 +0530

    added followers table

--- app/models.py
+++ app/models.py
@@ -5,6 +5,12 @@ from flask_login import UserMixin
 from hashlib import md5
 
 
+followers = db.Table("followers",
+                     db.Column("follower_id", db.Integer, db.ForeignKey("user.id")),
+                     db.Column("followed_id", db.Integer, db.ForeignKey("user.id"))
+                     )
+
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -12,7 +18,13 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    posts = db.relationship("Post", backref="author", lazy="dynamic")
+    followed = db.relationship(
+        "User", secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref("followers", lazy="dynamic"), lazy="dynamic"
+    )
 
     def __repr__(self):
         return f"<User {self.username}>"
