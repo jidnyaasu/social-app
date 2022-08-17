3a7471aca9ae68baa92e7bea9984e131a05ff3e0
Saurabh <sbj.amb@gmail.com>
2022-08-17 18:09:19 +0530

    Posts retrieved for self and followed

--- app/models.py
+++ app/models.py
@@ -50,6 +50,13 @@ class User(UserMixin, db.Model):
     def is_following(self, user):
         return self.followed.filter(followers.c.followed_id == user.id).count() > 0
 
+    def followed_posts(self):
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+            followers.c.follower_id == self.id)
+        own = Post.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Post.timestamp.desc())
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
