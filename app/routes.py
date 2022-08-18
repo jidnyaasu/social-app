4ae06072d32294fc6395491495f12c1c8384cfe7
Saurabh <sbj.amb@gmail.com>
2022-08-18 14:55:15 +0530

    Added explore page

--- app/routes.py
+++ app/routes.py
@@ -131,3 +131,10 @@ def unfollow(username):
         return redirect(url_for("user", username=username))
     else:
         return redirect(url_for("index"))
+
+
+@app.route("/explore")
+@login_required
+def explore():
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template("index.html", title="Explore", posts=posts)
