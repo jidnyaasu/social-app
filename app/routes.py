22cf83bcb1ed1e6b790b131e37be1ac2d6a08411
Saurabh <sbj.amb@gmail.com>
2022-08-18 20:26:54 +0530

    Pagination

--- app/routes.py
+++ app/routes.py
@@ -28,7 +28,11 @@ def index():
     page = request.args.get("page", 1, type=int)
     posts = current_user.followed_posts().paginate(
         page, app.config["POSTS_PER_PAGE"], False)
-    return render_template("index.html", title="Home", posts=posts.items, form=form)
+    next_url = url_for("index", page=posts.next_num) if posts.has_next else None
+    prev_url = url_for("index", page=posts.prev_num) if posts.has_prev else None
+    return render_template("index.html", title="Home",
+                           posts=posts.items, form=form,
+                           next_url=next_url, prev_url=prev_url)
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -74,9 +78,16 @@ def register():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = Post.query.filter_by(author=current_user).order_by(Post.timestamp.desc())
+    page = request.args.get("page", 1, type=int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page, app.config["POSTS_PER_PAGE"], False)
+    next_url = url_for("user", username=user.username, page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for("user", username=user.username, page=posts.prev_num) \
+        if posts.has_prev else None
     form = Emptyform()
-    return render_template("user.html", user=user, posts=posts, form=form)
+    return render_template("user.html", user=user, posts=posts.items, form=form,
+                           next_url=next_url, prev_url=prev_url)
 
 
 @app.route("/edit_profile", methods=["GET", "POST"])
@@ -141,4 +152,7 @@ def explore():
     page = request.args.get("page", 1, type=int)
     posts = Post.query.order_by(Post.timestamp.desc()).paginate(
         page, app.config["POSTS_PER_PAGE"], False)
-    return render_template("index.html", title="Explore", posts=posts.items)
+    next_url = url_for("explore", page=posts.next_num) if posts.has_next else None
+    prev_url = url_for("explore", page=posts.prev_num) if posts.has_prev else None
+    return render_template("index.html", title="Explore",
+                           posts=posts.items, next_url=next_url, prev_url=prev_url)
