e8e9bc20de1bcf3a87ebfbafe123ca1ea6204afc
Saurabh <sbj.amb@gmail.com>
2022-08-20 12:02:24 +0530

    Applying bootstrap css

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ from flask_login import LoginManager
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -15,7 +16,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = "login"
 mail = Mail(app)
-
+bootstrap = Bootstrap(app)
 
 if not app.debug:
     if not os.path.exists("logs"):
